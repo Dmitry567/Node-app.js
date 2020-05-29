62d5bbdb18d2da61f9af20fe49cd682223de1ed7
dfavo <favorskyd@gmail.com>
2020-05-29 13:02:45 -0700

    Refactoring API Features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,57 +1,23 @@
 const Tour = require('./../models/tourModel');
+const APIFeatures = require('./../utils/apiFeaters');
+
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+}
 
 exports.getAllTours = async (req, res) => {
   try {
-    console.log(req.query)
-    // BUILD QUERY
-    // 1A) Filtering
-    const queryObj = {...req.query};
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach(el => delete queryObj[el])
-
-    // 1B) Advanced filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);// Regular Expression
-
-    // { difficulty: 'easy', duration: { $gte: 5} } $ it is MongoDB operator sing
-    // { difficulty: 'easy', duration: { gte: '5' } }
-    // gte- greater than or equal, gt-greater than, lte-less than or equal, lt-less than
-
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // 2) Sorting
-    if(req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-     query = query.sort('-createdAt');
-    }
-
-    // 3) Field limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields)
-    } else {
-      query = query.select('-__v');
-    }
-
-    // 4) Pagination
-     const page = req.query.page * 1 || 1;
-     const limit = req.query.limit * 1 || 100;
-     const skip = (page - 1) * limit;
-
-
-    // page=2&limit=10, 1-10, page 1, 11-20, page 2, 21-30 page 3
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exist');
-    }
-
 
     // EXECUTE THE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+        .filter()
+        .sort()
+        .limitFields()
+        .paginate();
+    const tours = await features.query;
     // query.sort().select().skip().limit()
 
     // find() method returns the query this is the reason why we can not use chaining here
@@ -204,3 +170,8 @@ exports.deleteTour = async (req, res) => {
 // const id = req.params.id * 1;
 
 // const tour = tours.find(el => el.id === id)
+
+
+
+
+
