bb6cffd1f646980b18ddc98fb8960d8544934ba7
dfavo <favorskyd@gmail.com>
2020-05-28 19:41:16 -0700

    Add Limiting Fields and Pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,22 +2,64 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
+    console.log(req.query)
     // BUILD QUERY
+    // 1A) Filtering
     const queryObj = {...req.query};
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach(el => delete queryObj[el])
 
-    const query = Tour.find(queryObj);
+    // 1B) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);// Regular Expression
 
-// find() method returns the query this is the reason why we can not use chaining here
+    // { difficulty: 'easy', duration: { $gte: 5} } $ it is MongoDB operator sing
+    // { difficulty: 'easy', duration: { gte: '5' } }
+    // gte- greater than or equal, gt-greater than, lte-less than or equal, lt-less than
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    if(req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+     query = query.sort('-createdAt');
+    }
+
+    // 3) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields)
+    } else {
+      query = query.select('-__v');
+    }
+
+    // 4) Pagination
+     const page = req.query.page * 1 || 1;
+     const limit = req.query.limit * 1 || 100;
+     const skip = (page - 1) * limit;
+
+
+    // page=2&limit=10, 1-10, page 1, 11-20, page 2, 21-30 page 3
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
+
+    // EXECUTE THE QUERY
+    const tours = await query;
+    // query.sort().select().skip().limit()
+
+    // find() method returns the query this is the reason why we can not use chaining here
     // const tours = Tour.find()
     //     .where('duration')
     //     .equals(5)
     //     .where('difficulty')
     //     .equals('easy')
-
-    // EXECUTE THE QUERY
-    const tours = await query;
 
     // SEND RESPONSE
     res.status(200).json({
