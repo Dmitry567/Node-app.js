aa0a062458b5cbeeb680553587250eb846c07a11
dfavo <favorskyd@gmail.com>
2020-06-05 20:48:55 -0700

    Handling Invalid Database IDs

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,8 @@
 const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/apiFeaters');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -39,6 +41,10 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 exports.getTour = catchAsync(async (req, res, next) => {
     const tour = await Tour.findById(req.params.id);
     // Tour.findOne({ _id: req.params.id})
+
+    if (!tour) {
+       return next(new AppError('No tour found with that ID', 404))
+    }
 
     res.status(200).json({
       status: 'success',
@@ -82,6 +88,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
       runValidators: true,
     });
 
+    if (!tour) {
+        return next(new AppError('No tour found with that ID', 404))
+    }
+
     res.status(200).json({
       status: 'success',
       data: {
@@ -91,7 +101,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-    await Tour.findByIdAndDelete(req.params.id);
+   const tour = await Tour.findByIdAndDelete(req.params.id);
+
+    if (!tour) {
+        return next(new AppError('No tour found with that ID', 404))
+    }
 
     res.status(204).json({
       status: 'success',
