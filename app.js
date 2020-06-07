b40c2879afdeccd7fdc8fa072da6abe86bc6b41a
dfavo <favorskyd@gmail.com>
2020-06-06 21:03:27 -0700

    Did Catching Uncaught Exceptions

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use((req, res, next) => {
   next();
 });
 
+
 // 3 ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+    console.log('UNCAUGHT EXCEPTION! Shutting down...');
+    console.log(err.name, err.message);
+    process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -18,12 +24,25 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false,
   })
-  .then(() => console.log('DB connection successful!'));
+    .then(() => console.log('DB connection successful!'));
+    //.catch(err => console.log('ERROR')); We can use catch if we want catch unhandled rejection error
 
 
 // env it is Global  variable
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+process.on('unhandledRejection', err => {
+    console.log('UNHANDLED REJECTION! Shutting down...');
+    console.log(err.name, err.message);
+    server.close(() => {// In a real world scenario we should do always like this
+        process.exit(1)// If we want crashed our app
+    });
+});
+
+
+
+
 
