f8dc4695ab635d9a1f886747bd362bdb7a85430a
dfavo <favorskyd@gmail.com>
2020-06-12 12:02:03 -0700

    More Protection for Tour Routes

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -20,6 +20,12 @@ const handleValidationErrorDB = err => {
   return new AppError(message, 400);
 }
 
+const handleJWTError = () =>
+    new AppError('Invalid token. Please log in again!', 401)
+//We should run server in production mode
+
+const handleJWTExpiredError = () =>
+    new AppError('Your token has expired! Please log in again.', 401);
 
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
@@ -69,6 +75,8 @@ module.exports = (err, req, res, next) => {
         if(error.name === 'ValidationError')
 
             error = handleValidationErrorDB(error);
+        if (error.name === 'JsonWebTokenError') error = handleJWTError();
+        if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
 
 
             sendErrorProd(error, res);
