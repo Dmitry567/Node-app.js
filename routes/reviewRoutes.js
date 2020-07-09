60d0920b8b01d06cb17c90b295043bf82bca88b4
dfavo <favorskyd@gmail.com>
2020-07-08 19:23:48 -0700

    Handler Factory Function Delete

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -5,6 +5,7 @@ const authController = require('./../controllers/authController');
 const router = express.Router({ mergeParams: true });
 
 // POST /tour/234fad4/reviews
+// GET /tour/234fad4/reviews
 // POST /reviews
 
 router
@@ -15,5 +16,7 @@ router
         authController.restrictTo('user'),
         reviewController.createReview
     );
+
+router.route('/:id').delete(reviewController.deleteReview);
 
 module.exports = router;
